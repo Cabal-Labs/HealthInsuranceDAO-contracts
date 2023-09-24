957fa6bbcdc47879f240f014ca1a1a7567e8c405
wjordansun <wjordansun@gmail.com>
2023-09-24 04:07:34 -0400

    took out imports

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.4;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-
 contract Treasury {
 
     struct Claim {
